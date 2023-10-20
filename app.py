4ee87c636e51201eadfa5cec53b016fd20ba2b41
AntKov <anton.beskyd@gmail.com>
2023-10-20 22:58:14 +0300

    todayDoW=7

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ app =  Flask(__name__)
 @app.route("/")
 
 def hellow_world():
- 
-  todayDoW=7-date.today().strftime('%u')
+ todayDoW=7
+  #todayDoW=7-date.today().strftime('%u')
 #  return f"<p> Today is {date.today().strftime('%d/%m/%Y  %A')}</p>   "
 #  <p> it is {todayDoW} till next Sunay </p> 
   return f"<p> {todayDoW}</p>   "
