49169852e7286ab8123704451380874ce2eaddbb
AntKov <anton.beskyd@gmail.com>
2023-11-06 00:28:53 +0200

    Update app.py

--- app.py
+++ app.py
@@ -8,9 +8,8 @@ app =  Flask(__name__)
 
 def hellow_world():
 
-toSaturday=7-int(date.today().strftime('%u'))
-
-if  toSaturday==0:
- return f"<p> Today is Saturday </p>   "
-else: 
- return f"<p> {toSaturday}  day(s) left until the nearest Saturday</p>"
+ toSaturday=7-int(date.today().strftime('%u'))
+ if  toSaturday==0:
+  return f"<p> Today is Saturday </p>   "
+ else: 
+  return f"<p> {toSaturday}  day(s) left until the nearest Saturday</p>"
